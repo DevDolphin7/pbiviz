cbc3d76b2c35af9b7d5076c0a1e6af363750087d
DevDolphin7 <devdolphin7@outlook.com>
2026-02-28 09:25:36 +0000

    Set up sandbox and connect it to PBI

---
+++ twVisual/src/App.tsx
@@ -0,0 +1,17 @@
+// import * as React from "react";
+
+export function App() {
+  return (
+    <div
+      style={{
+        display: "grid",
+        placeItems: "center",
+        height: "100vh",
+        fontSize: "24px",
+        color: "blue",
+      }}
+    >
+      Hello, world!
+    </div>
+  );
+}
